03a610a401a4d4f509e1cfad3fb486ffa9f5eb57
community <2463197762@qq.com>
2020-04-24 14:06:46 +0800

    授权

---
+++ src/main/java/com/txx/demo/controller/AuthrizeController.java
@@ -0,0 +1,30 @@
+package com.txx.demo.controller;
+
+import com.txx.demo.dto.AcessTokenDto;
+import com.txx.demo.dto.GithubUser;
+import com.txx.demo.utils.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthrizeController {
+    @Autowired
+    private GitHubProvider gitHubProvider;
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state")String state){
+        AcessTokenDto acessTokenDto=new AcessTokenDto();
+         acessTokenDto.setClient_id("b79f62cb35784eb3d84b");
+         acessTokenDto.setClient_secret("05a532f517d8006b08ae58245f867873343123ea");
+         acessTokenDto.setCode(code);
+         acessTokenDto.setRedirect_uri("http://localhost:8080/callback");
+         acessTokenDto.setState(state);
+         gitHubProvider.getAccessToken(acessTokenDto);
+        return "index";
+    }
+    public GithubUser getUser(String access_token){
+
+    }
+}
---
+++ src/main/java/com/txx/demo/utils/GitHubProvider.java
@@ -0,0 +1,33 @@
+package com.txx.demo.utils;
+
+import com.txx.demo.dto.AcessTokenDto;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+/*
+*
+*
+*/
+@Component
+public class GitHubProvider {
+    public String  getAccessToken(AcessTokenDto acessTokenDto){
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()//??
+                .url("https://github.com/login/oauth/access_token")//???
+                .post(body)
+                .build();
+
+            try (Response response = client.newCall(request).execute()) {//???
+                String string=response.body().string();
+                System.out.println(string);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+              return null;
+
+    }
+}
